86bdc83d6ddd0b0739b2a6c035e5235c1fa0b14f
Ivan Dimitrov <idimitrov07@gmail.com>
2014-02-22 16:38:44 +0200

    add events, edit events

--- client/calendar.js
+++ client/calendar.js
@@ -1,11 +1,28 @@
 CalEvents = new Meteor.Collection('calevents');
-Session.setDefault('editing_event', null);
+Session.setDefault('editing_calevent', null);
 Session.setDefault('showEditEvent', false);
 Session.setDefault('lastMod', null);
 Meteor.Router.add({
   '/':'homepage',
   '/calendar':'calendar'
 })
+
+Template.calendar.showEditEvent = function(){
+  return Session.get('showEditEvent');
+}
+
+Template.editEvent.evt = function(){
+  return CalEvents.findOne({_id:Session.get('editing_calevent')});
+}
+
+Template.editEvent.events({
+  'click .save': function(evt, tmpl){
+    updateCalEvent(Session.get('editing_calevent', tmpl.find('.title').value));
+    Session.set('editing_calevent', null);
+    Session.set('showEditEvent', false);
+  }
+});
+
 Template.calendar.rendered = function() {
   $('#calendar').fullCalendar({
       dayClick:function(date, allDay, jsEvent, view) {
@@ -13,6 +30,8 @@ Template.calendar.rendered = function() {
         Session.set('lastMod', new Date());
       },
       eventClick:function(calEvent, jsEvent, view){
+        Session.set('editing_calevent', calEvent.id);
+        Session.set('showEditEvent', true);
 
       },
       events:function(start, end, callback){
@@ -32,4 +51,9 @@ Template.calendar.rendered = function() {
 }
 Template.calendar.lastMod = function(){
   return Session.get('lastMod');
+}
+
+var updateCalEvent = function(id, title){
+  CalEvents.update(id, {$set: {title:title}});
+  return true;
 }
